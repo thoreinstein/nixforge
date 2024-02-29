db3cebb326fea83181d7cb1c8e7f0808f9c50644
Jim Anders <thoreinstein8@gmail.com>
2024-02-28 22:04:14 -0500

    Update the neovim dev template

--- modules/neovim/shell.nix
+++ modules/neovim/shell.nix
@@ -1,18 +1,29 @@
-{pkgs ? import <nixpkgs> {}}:
+{
+  pkgs ? import <nixpkgs> {},
+  buildInputs ? [],
+  shellHook ? "",
+  ...
+}:
 with pkgs; let
   luaFile = callPackage ./lua.nix {inherit pkgs;};
+  newBuildInputs = buildInputs;
+  newShellHook = shellHook;
 in
   mkShell {
-    buildInputs = [
-      alejandra
-      luaFile
-      lua-language-server
-      stylua
-    ];
+    buildInputs =
+      [
+        alejandra
+        luaFile
+        lua-language-server
+        stylua
+      ]
+      ++ newBuildInputs;
 
-    shellHook = ''
-      cp ${luaFile}/.nvim.lua ./
+    shellHook =
+      ''
+        cp ${luaFile}/.nvim.lua ./
 
-      trap 'rm -f ./.nvim.lua' EXIT
-    '';
+        trap 'rm -f ./.nvim.lua' EXIT
+      ''
+      + newShellHook;
   }
