ffd7218d648179091df348584191ca4ab5dae5ab
Jim Anders <thoreinstein8@gmail.com>
2024-03-06 21:02:44 -0500

    Correct typo in rust shell

--- modules/nixos/shell.nix
+++ modules/nixos/shell.nix
@@ -9,26 +9,28 @@
 in
   with pkgs;
     mkShell {
-      buildInputs = [alejandra luaFile nil codespell nodePackages.bash-language-server nixfmt shellharden];
+      buildInputs = [alejandra luaFile nil codespell nodePackages.bash-language-server nixfmt shellharden] ++ newBuildInputs;
 
-      shellHook = ''
-        lua_file=".nvim.lua"
-        lock_file="/tmp/nixforge-cloud"
+      shellHook =
+        ''
+          lua_file=".nvim.lua"
+          lock_file="/tmp/nixforge-cloud"
 
-        cleanup() {
-          if [ -f "$lock_file" ] && [ "$(cat $lock_file)" = "$$" ]; then
-            rm -f "$lua_file"
-            rm -f "$lock_file"
+          cleanup() {
+            if [ -f "$lock_file" ] && [ "$(cat $lock_file)" = "$$" ]; then
+              rm -f "$lua_file"
+              rm -f "$lock_file"
+            fi
+          }
+
+          if [ ! -f "$lua_file" ]; then
+            cp ${luaFile}/"$lua_file" ./
           fi
-        }
 
-        if [ ! -f "$lua_file" ]; then
-          cp ${luaFile}/"$lua_file" ./
-        fi
-
-        if [ ! -f "$lock_file" ]; then
-          echo $$ > "$lock_file"
-          trap cleanup EXIT
-        fi
-      '';
+          if [ ! -f "$lock_file" ]; then
+            echo $$ > "$lock_file"
+            trap cleanup EXIT
+          fi
+        ''
+        + newShellHook;
     }
--- modules/rust/shell.nix
+++ modules/rust/shell.nix
@@ -36,5 +36,5 @@ in
           fi
 
         ''
-        ++ newShellHook;
+        + newShellHook;
     }
